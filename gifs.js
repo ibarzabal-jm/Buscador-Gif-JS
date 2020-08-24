c34a84fab11664f52a2c27392bfab56058ebf0c5
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-08-23 22:34:14 -0300

    Update gifs.js

--- gifs.js
+++ gifs.js
@@ -3,17 +3,22 @@ btnCargarListado.addEventListener('click', cargarListadoGifs );
 
 
 
-let apiKey = 'Hhli4pP1vrJ8QgAFm85OogcEUA0bw1Xw';
-let url = 'https://api.giphy.com/v1/gifs/search'; 
+
 
 
 
 
 async function getGifs(){
 
+	let apiKey = 'Hhli4pP1vrJ8QgAFm85OogcEUA0bw1Xw';
+	let url = 'https://api.giphy.com/v1/gifs/search';
+
+	let cantidad = 5;
+
+
 	let terminoBuscado = document.getElementById('txt-busqueda').value;
 
-	url += `?api_key=${apiKey}&q=${terminoBuscado}&limit=1`;
+	url += `?api_key=${apiKey}&q=${terminoBuscado}&limit=${cantidad}`;
 
 	let response = await fetch( url );
 	let datos    = await response.json();
@@ -34,7 +39,6 @@ function cargarListadoGifs(){
 
 function mostrarGifs ( gifs ){
 	
-	console.log(gifs);
 
 	let listadoGifs= document.getElementById('listado-Gifs');
 
